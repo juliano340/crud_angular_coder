1fba463bf25b98063eb7f7ac6b281bb191627ad8
Juliano Pereira <juliano340@gmail.com>
2024-03-22 13:10:15 -0300

    iniciando nav component...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/template/header/header.component";
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+
 
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: 'app.component.html',
-    imports: [RouterOutlet, HeaderComponent, MatToolbarModule, FooterComponent],
+    imports: [RouterOutlet, HeaderComponent, MatToolbarModule, FooterComponent,MatSidenavModule, MatListModule],
 })
 export class AppComponent {
   
