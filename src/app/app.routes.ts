b52e4a31840af70d44f595fc3a8344d0456bd609
Juliano Pereira <juliano340@gmail.com>
2024-03-27 21:07:49 -0300

    Add Product Create Component and Route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component'
 import { ProductCrudComponent } from './views/product-crud/product-crud.component'
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
 export const routes: Routes = [{
     path: '',
@@ -10,6 +11,10 @@ export const routes: Routes = [{
 {
     path: 'products',
     component: ProductCrudComponent
+},
+{
+    path: 'products/create',
+    component: ProductCreateComponent
 }
 
 ];
