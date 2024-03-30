bec0e9d91f90b8c358b5cb7b8413c227868c4c0a
Juliano Pereira <juliano340@gmail.com>
2024-03-30 14:25:01 -0300

    Add table html and methods uptade ant read by id.

--- src/app/components/product/product-read/product-read.component.ts
+++ src/app/components/product/product-read/product-read.component.ts
@@ -3,11 +3,12 @@ import { Product } from '../product-model';
 import { ProductService } from '../product.service';
 import {CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-read',
   standalone: true,
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, MatTableModule, RouterLink],
   templateUrl: './product-read.component.html',
   styleUrl: './product-read.component.css'
 })
@@ -15,7 +16,7 @@ export class ProductReadComponent implements OnInit {
 
   products: Product[] = []; 
 
-  displayedColumns = ['id', 'name', 'price'];
+  displayedColumns = ['id', 'name', 'price', 'action'];
   
   constructor(private productService: ProductService) { }
   ngOnInit(): void {
